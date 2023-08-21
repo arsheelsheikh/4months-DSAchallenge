4125f44f3795d40f42c5c044faac71b04852cb19
arsheelsheikh <sheikharsheel@gmail.com>
2023-08-21 19:21:23 +0530

    287 cyclic sort sol

---
+++ Cpp/Sorting/287-Find-the-Duplicate-Number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int i = 0;
+        int n = nums.size();
+        
+        while (i < n) {
+            if (nums[i] != i + 1) {
+                int correct = nums[i] - 1;
+                if (nums[i] != nums[correct]) {
+                    swap(nums, i, correct); 
+                } else {
+                    return nums[i];
+                }
+            } else {
+                i++; 
+            }
+        }
+        
+        return -1; 
+    }
+
+    // Swap function
+    static void swap(std::vector<int>& nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+};
