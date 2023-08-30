37d07fafda4fbd576596adf874aad47fbfa3bfb4
arsheelsheikh <sheikharsheel@gmail.com>
2023-08-31 05:10:50 +0530

    capitalization check using isupper and islower functions

--- Cpp/Strings/520-Detect-Capital.cpp
+++ Cpp/Strings/520-Detect-Capital.cpp
@@ -2,11 +2,15 @@ class Solution {
 public:
     bool detectCapitalUse(string word) {
         for (int i = 1; i < word.length(); i++) {
-            if (isupper(word[1]) != isupper(word[i])) {
-                return false; // Capitalization rule violated
+            // Check if the capitalization of the second character is different from the current character.
+            // Also, check if the first character is lowercase and the current character is uppercase.
+            if (isupper(word[1]) != isupper(word[i]) ||
+                islower(word[0]) && isupper(word[i])) {
+                // If any of the two capitalization rules are violated, return false.
+                return false;
             }
         }
-        
-        return true; // Capitalization follows the rules
+        // If the loop completes without finding any violations, return true.
+        return true;
     }
 };
