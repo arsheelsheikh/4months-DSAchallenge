9665499d319a6312163975d00f33e95dbb92ce94
arsheelsheikh <sheikharsheel@gmail.com>
2023-09-01 15:21:33 +0530

    reverseWords

--- Cpp/Strings/557-Reverse-Words-in-a-String-III.cpp
+++ Cpp/Strings/557-Reverse-Words-in-a-String-III.cpp
@@ -4,12 +4,14 @@ public:
         int start = 0;
         int n = str.length();
         for (int i = 0; i <= n; i++) {
+            // Check if the current character is a space or if we have reached the end of the string.
             if (str[i] == ' ' || i == n) {
-                int end = i - 1;
+                int end = i - 1; // Calculate the ending position of the current word.
+                // Reverse the characters within the current word (from start to end).
                 while (start < end) {
                     swap(str[start++], str[end--]);
                 }
-                start = i + 1;
+                start = i + 1; // Update the starting position for the next word.
             }
         }
         return str;
