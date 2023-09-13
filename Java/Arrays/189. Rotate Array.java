e823d82afa37beefce7883e7f0bb77db084ef84a
arsheelsheikh <sheikharsheel@gmail.com>
2023-09-13 23:59:31 +0530

    array

---
+++ Java/Arrays/189. Rotate Array.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        
+        // Reverse the entire array
+        reverse(nums, 0, nums.length - 1);
+        
+        // first part up to k-1
+        reverse(nums, 0, k - 1);
+        
+        // remaining part from k to the end
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
