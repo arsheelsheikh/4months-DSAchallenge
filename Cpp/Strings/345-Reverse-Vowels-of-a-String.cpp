fcecb57eb361ecad24eb9ab3c08c28954f4807b0
arsheelsheikh <sheikharsheel@gmail.com>
2023-09-16 19:33:25 +0530

    strings, two pointers

--- Cpp/Strings/345-Reverse-Vowels-of-a-String.cpp
+++ Cpp/Strings/345-Reverse-Vowels-of-a-String.cpp
@@ -28,3 +28,56 @@ public:
         return s;
     }
 };
+
+//2)
+class Solution {
+public:
+    bool isvowel(char ch){
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A'|| ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') 
+            return true;
+        else return false;
+    }
+    
+    string reverseVowels(string s) {
+        int start =0;
+        int end = s.size()-1;
+        
+        while(start<end){
+            if(isvowel(s[start]) && isvowel(s[end])){
+                swap(s[start++],s[end--]);
+            }
+            if(!isvowel(s[start]) ){
+                start++;
+            }
+            if(!isvowel(s[end]) ){
+                end--;
+            }
+        }
+        return s;
+    }
+};
+
+//3)
+class Solution {
+ public:
+  string reverseVowels(string s) {
+    int l = 0;
+    int r = s.length() - 1;
+
+    while (l < r) {
+      while (l < r && !isVowel(s[l]))
+        ++l;
+      while (l < r && !isVowel(s[r]))
+        --r;
+      swap(s[l++], s[r--]);
+    }
+
+    return s;
+  }
+
+ private:
+  bool isVowel(char c) {
+    static constexpr string_view kVowels = "aeiouAEIOU";
+    return kVowels.find(c) != string_view::npos;
+  }
+};
