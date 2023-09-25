462959776a774da10ff54400c31d18901aca5fa1
arsheelsheikh <sheikharsheel@gmail.com>
2023-09-25 18:21:16 +0530

    array sorting

---
+++ Java/Sorting/1051. Height Checker.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] expected = Arrays.copyOf(heights, heights.length);
+        Arrays.sort(heights);
+
+        int n = heights.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (heights[i] != expected[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
