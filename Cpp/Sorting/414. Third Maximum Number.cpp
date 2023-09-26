e872e88f0f7e0117b96859f1e25c88356ab4ea06
arsheelsheikh <sheikharsheel@gmail.com>
2023-09-26 21:17:13 +0530

    array sorting

---
+++ Cpp/Sorting/414. Third Maximum Number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+  int thirdMax(vector<int>& nums) {
+    long max1 = LONG_MIN;  // The maximum
+    long max2 = LONG_MIN;  // 2nd maximum
+    long max3 = LONG_MIN;  // 3rd maximum
+
+    for (const int num : nums)
+      if (num > max1) {
+        max3 = max2;
+        max2 = max1;
+        max1 = num;
+      } else if (max1 > num && num > max2) {
+        max3 = max2;
+        max2 = num;
+      } else if (max2 > num && num > max3) {
+        max3 = num;
+      }
+
+    if (max3 == LONG_MIN) {
+      return max1;
+    }
+
+    return max3;
+  }
+};
