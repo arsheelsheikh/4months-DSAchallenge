e17be8e2838f9f516e312e7714edd19efeaa6521
arsheelsheikh <sheikharsheel@gmail.com>
2023-10-26 23:59:06 +0530

    linked list M

---
+++ Cpp/Linked List/2. Add Two Numbers.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* p = l1, *q = l2, *current = dummyHead;
+        int carry = 0;
+
+        while (p != nullptr || q != nullptr) {
+            int x = (p != nullptr) ? p->val : 0;
+            int y = (q != nullptr) ? q->val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+            if (p != nullptr) p = p->next;
+            if (q != nullptr) q = q->next;
+        }
+
+        if (carry > 0) {
+            current->next = new ListNode(carry);
+        }
+
+        return dummyHead->next;
+    }
+};
